cef2dfe689c107f8c0e9c6c4e03105d7d3d67d2c
Victor desktop <vflorespineda@gmail.com>
2018-03-05 08:56:17 -0600

    Shell creation min 18 40

--- Tutorial_tanques/Assets/Scripts/Shell/ShellExplosion.cs
+++ Tutorial_tanques/Assets/Scripts/Shell/ShellExplosion.cs
@@ -20,6 +20,19 @@ public class ShellExplosion : MonoBehaviour
     private void OnTriggerEnter(Collider other)
     {
         // Find all the tanks in an area around the shell and damage them.
+        Collider[] colliders = Physics.OverlapSphere(transform.position, m_ExplosionRadius, m_TankMask);
+        
+        for(int i=0; i<colliders.Length; i++){
+            Rigidbody targetRigidbody = colliders[i].GetComponent<Rigidbody>();
+            if(!targetRigidbody)
+                continue;
+
+            targetRigidbody.AddExplosionForce(m_ExplosionForce, transform.position, m_ExplosionRadius);
+            TankHealth targetHealth = targetRigidbody.GetComponent<TankHealth>();
+
+            if(!targetHealth)
+                continue;
+        }
     }
 
 
